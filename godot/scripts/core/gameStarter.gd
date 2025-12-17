5cdbb722478b5ff6d96250010dd588b1619fc21d
fatcatfarts <agrimbhanot09@gmail.com>
2025-12-17 12:10:37 +0530

    feat(v0 updates): added a title screen and enhanced gameplay

--- godot/scripts/core/gameStarter.gd
+++ godot/scripts/core/gameStarter.gd
@@ -1,24 +1,17 @@
 extends Node2D
-@onready var Master: HSlider = $Camera2D/Master
-@onready var Background: HSlider = $Camera2D/Background
-@onready var Sfx: HSlider = $Camera2D/SFX
+var player_camera_scene := preload("res://scenes/world/Camera.tscn")
 
-var background = preload("res://Music/We Rollin - Djjohal.fm.mp3")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	AudioManager.play_audio(background,1)
 	if PlayerGlobal.player_instance == null:
 		print("Starting instantiation of player")
 		var player = PlayerGlobal.player_scene.instantiate()
 		add_child(player)
 		PlayerGlobal.player_instance = player
 		print("Instantiated player")
-		var dengey = PlayerGlobal.player_instance
-		print(dengey);
-
-func _process(delta: float) -> void:
-	AudioManager.set_master_vol(Master.value/100)
-	AudioManager.set_background_vol(Background.value/100)
-	AudioManager.set_SFX_vol(Sfx.value/100)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+		var MainPlayerNode = PlayerGlobal.player_instance.CharacterBody
+		var camera = player_camera_scene.instantiate()
+		MainPlayerNode.add_child(camera)
+		camera.make_current()
+		camera.zoom = Vector2(2,2)
+		camera.position.y -= 75
--- godot/scripts/core/ui_scripts/sampleSaveLoad.gd
+++ godot/scripts/core/ui_scripts/sampleSaveLoad.gd
@@ -1,25 +1,31 @@
-extends Button
+extends Control
+@onready var save: Button = $save
+@onready var master: HSlider = $Master
+@onready var background: HSlider = $Background
+@onready var sfx: HSlider = $SFX
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	save.process_mode = Node.PROCESS_MODE_ALWAYS
 	process_mode = Node.PROCESS_MODE_ALWAYS
-	pass # Replace with function body.
-
+	var volArr = AudioManager.ReturnLastVolArray()
+	master.value = volArr[0]*100
+	background.value = volArr[1]*100
+	sfx.value = volArr[2]*100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
-func _on_pressed() -> void:
-	SaveManagerGlobal.save_game();
-	
-
-
-
-
-
 func _on_load_pressed() -> void:
 	SaveManagerGlobal.load_game();
-	pass # Replace with function body.
+
+
+func _on_save_pressed() -> void:
+	SaveManagerGlobal.save_game();
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
